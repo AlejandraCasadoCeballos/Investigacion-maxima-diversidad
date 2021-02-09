73e85c7ffe46163eb83a060d2a42001962317e2e
pedro876 <pedrocasas876@gmail.com>
2021-02-09 12:19:50 +0100

    lectura de instancias completa

--- src/momdp/Main.java
+++ src/momdp/Main.java
@@ -1,21 +1,70 @@
 package momdp;
 
+import momdp.structure.Instance;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
-    private String pathFolder= "./instances";
+    final static String pathFolder= "./instances";
+    static ArrayList<Instance> instances;
 
-    private void readFolder(){
+    final static boolean readAllFolders = false;
+    final static boolean readAllInstances = false;
+    final static String folderIndex = "GKD-a";
+    final static String instanceIndex = "GKD-a_1_n10_m2.txt";
+
+    static List<String> foldersNames;
+    static List<String> instancesNames;
+    static String instanceFolderPath;
+
+    final public static boolean DEBUG = true;
+
+    public static void main(String[] args){
+        readData();
+    }
+
+    private static void readData(){
+        instances = new ArrayList<>();
+        foldersNames = Arrays.asList(new File(pathFolder).list());
+
+
+        if(readAllFolders) readAllFolders();
+        else if (foldersNames.contains(folderIndex)) readFolder(folderIndex);
+        else System.out.println("Folder index exceeds the bounds of the array");
+    }
+
+    private static void readAllFolders(){
+        instances = new ArrayList<>();
         String [] folders =new File(pathFolder).list();
-        File file;
 
-        for (String f:folders) {
-            file=new File(pathFolder+"/"+f);
-            if(!f.startsWith(".") && !f.startsWith("..") && file.isDirectory()){
-                
-            }
+        for(String fileName : folders){
+            readFolder(fileName);
         }
+    }
 
+    private static void readFolder(String fileName){
+        File file;
+        file=new File(pathFolder+"/"+fileName);
+        if(!fileName.startsWith(".") && !fileName.startsWith("..") && file.isDirectory()){
+            instancesNames = Arrays.asList(file.list());
+            instanceFolderPath = file.getPath() + "/";
+            if(readAllInstances) readAllInstances();
+            else if (instancesNames.contains(instanceIndex)) readInstance(instanceIndex);
+            else System.out.println("Instance index exceeds the bounds of the array");
+        }
+    }
+
+    private static void readAllInstances(){
+        for(String instanceName : instancesNames){
+            readInstance(instanceName);
+        }
+    }
+
+    private static void readInstance(String instanceName){
+        instances.add(new Instance(instanceName,instanceFolderPath +instanceName));
     }
 }
--- src/momdp/structure/Instance.java
+++ src/momdp/structure/Instance.java
@@ -1,11 +1,77 @@
 package momdp.structure;
+import momdp.Main;
+
+import java.io.*;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 
 public class Instance {
 
+    public String name;
+    public String path;
+    public int numNodes;
+    public int numNodesSol;
+    public float[][] distances;
+    float maxDistance = 0.0f;
 
-
-    private void readFile(){
-
+    public Instance(String name, String path){
+        this.path = path;
+        this.name = name;
+        readInstance();
     }
 
+    private void readInstance(){
+        try{
+            File file = new File(path);
+            Reader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            boolean firstLine = true;
+            String[] parts;
+            int nodeA;
+            int nodeB;
+            float distance;
+            if(Main.DEBUG)System.out.println("\nFILE " + name);
+            while((line = br.readLine()) != null){
+                if(Main.DEBUG) System.out.println(line);
+                parts = line.split(" ");
+                if(firstLine){
+                    firstLine = false;
+                    numNodes = Integer.parseInt(parts[0]);
+                    numNodesSol = Integer.parseInt(parts[1]);
+                    distances = new float[numNodes][numNodes];
+                } else {
+                    nodeA = Integer.parseInt(parts[0]);
+                    nodeB = Integer.parseInt(parts[1]);
+                    distance = Float.parseFloat(parts[2]);
+                    if(distance > maxDistance) maxDistance = distance;
+                    distances[nodeA][nodeB] = distance;
+                    distances[nodeB][nodeA] = distance;
+                }
+            }
+            if(Main.DEBUG)printDistancesMatrix();
+
+        } catch (FileNotFoundException e){
+            System.out.println(("File not found " + path));
+        } catch (IOException e){
+            System.out.println("Error reading line on " + name);
+        }
+    }
+
+    private void printDistancesMatrix(){
+        System.out.println("\nDISTANCE MATRIX OF " + name);
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+        symbols.setDecimalSeparator('.');
+        String zeros = "";
+        int numZeros = (""+(int)maxDistance).length();
+        for(int i = 0; i < numZeros; i++) zeros += '0';
+
+        for(float[] row : distances){
+            for(float value : row){
+                System.out.print(new DecimalFormat(zeros+".00000", symbols).format(value) + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
